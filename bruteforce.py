3ed518d2163dabbdca49f14ba073ed6616779515
mr-gantai <50599491+mr-gantai@users.noreply.github.com>
2019-05-14 06:22:45 +0700

    bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -8,33 +8,6 @@ import cookielib
 
 GHT = '''
 '''
-#version 1.0
-
-clear
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
-
-cowsay -f eyes "Moreno77" | lolcat
-figlet -f slant "Moreno77" | lolcat
-echo "    <=====================[]====================>" | lolcat
-echo "    <=====[       Tools By Moreno77       ]=====>" | lolcat
-echo  "    <=====[  Concact Me : +6285606753071  ]=====>" | lolcat
-echo "    <=====================[]====================>" | lolcat
-
-
 print "Gunakan Dengan Bijak"
 print "CTRL+C Untuk Keluar"
 print "Thanks To Mr.gantai"
